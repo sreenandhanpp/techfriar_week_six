ea45151ed129fc6bfe243603bba5892f779b9abd
sreenandhanpp <sreedeviv27801@gmail.com>
2023-10-02 11:32:37 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const __dirname = path.resolve();
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
