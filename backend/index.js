537a45cdb34bf2f132a2e99e0bdd262e386cb532
sreenandhanpp <sreedeviv27801@gmail.com>
2023-10-02 11:52:51 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'))
+);
+
 //creating user route
 
 
